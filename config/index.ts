d6d1c1e054578f9a753125c5991408e399d2ec99
rakibhossrn1212175-ai <rakibhossrn1212175@gmail.com>
2026-09-09 19:22:09 +0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,20 @@
 import type { AppInfo } from '@/types/app'
+
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'International Islamic AI',
+  description: 'International Islamic Library-এর জ্ঞানভাণ্ডারভিত্তিক ইসলামিক AI',
+  copyright: 'International Islamic Library',
   privacy_policy: '',
   default_language: 'en',
-  disable_session_same_site: false, // set it to true if you want to embed the chatbot in an iframe
+  disable_session_same_site: false,
 }
 
 export const isShowPrompt = false
+
 export const promptTemplate = 'I want you to act as a javascript console.'
 
 export const API_PREFIX = '/api'
